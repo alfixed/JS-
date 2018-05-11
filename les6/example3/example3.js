f613573936036dc79fc42b3ed839384a5f2bfa6f
alfixed <alfixed@gmail.com>
2018-05-11 22:36:25 +0300

    Add Lesson 6 2 part

--- les6/example3/example3.js
+++ les6/example3/example3.js
@@ -1,14 +1,3 @@
-// var result = fetch('https://api.github.com')
-// result.then(function(response) {
-//   console.log('response', response)
-//   console.log('header', response.headers.get('Content-Type'))
-//   return response.text()
-// }).then(function(text) {
-//   console.log('got text', text)
-// }).catch(function(ex) {
-//   console.log('failed', ex)
-// })
-
 let isError = false;
 let result1 = fetch('https://jsonplaceholder.typicode.com/users/1');
 let result2 = fetch('https://jsonplaceholder.typicode.com/users/2');
