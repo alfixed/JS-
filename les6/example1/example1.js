28a15bf9a12abc81f98d9db27b36bfbb4375aabe
alfixed <alfixed@gmail.com>
2018-05-11 22:35:26 +0300

    Add Lesson 6

--- les6/example1/example1.js
+++ les6/example1/example1.js
@@ -1,45 +1,13 @@
 const { readFile } = require('fs');
 
-const p1 = new Promise(
-  (resolve, reject) => {
-    readFile('test1.json', { encoding: 'utf8' },
-      (error, data) => {
-        if (error) {
-          reject(error);
-        } else {
-          resolve(data);
-        }
-      });
-  });
 
-const p2 = new Promise(
-  (resolve, reject) => {
-    readFile('test2.json', { encoding: 'utf8' },
-      (error, data) => {
-        if (error) {
-          reject(error);
-        } else {
-          resolve(data);
-        }
-      });
-  });
 
-const p3 = new Promise(
-  (resolve, reject) => {
-    readFile('test3.json', { encoding: 'utf8' },
-      (error, data) => {
-        if (error) {
-          reject(error);
-        } else {
-          resolve(data);
-        }
-      });
-  });
 
-function loadFileAsync(fileName) {
-  return new Promise(
+
+function tmpSync(){
+  const p1 = new Promise(
     (resolve, reject) => {
-      readFile(fileName, { encoding: 'utf8' },
+      readFile('test1.json', { encoding: 'utf8' },
         (error, data) => {
           if (error) {
             reject(error);
@@ -48,52 +16,72 @@ function loadFileAsync(fileName) {
           }
         });
     });
-}
-
-function tmpSync(){
+  
+  const p2 = new Promise(
+    (resolve, reject) => {
+      readFile('test2.json', { encoding: 'utf8' },
+        (error, data) => {
+          if (error) {
+            reject(error);
+          } else {
+            resolve(data);
+          }
+        });
+    });
+  
+  const p3 = new Promise(
+    (resolve, reject) => {
+      readFile('test3.json', { encoding: 'utf8' },
+        (error, data) => {
+          if (error) {
+            reject(error);
+          } else {
+            resolve(data);
+          }
+        });
+    });
+    
   p1.then(text => {
-      //console.log(text);
+    return p2;
   })
-  .catch(error => {
-      //console.log(error);
-  });
-
-  p2.then(text => {
-      //console.log(text);
+  .then(text => {
+    return p3;
   })
-  .catch(error => {
-      //console.log(error);
-  });
-
-  p3.then(text => {
-    //console.log(text);
-  })
-  .catch(error => {
-    //console.log(error);
-  });
+  .then(text => {})
+  .catch(error => {console.log(error);});
 }
 
 function tmpAsync(){
+  function loadFileAsync(fileName) {
+    return new Promise(
+      (resolve, reject) => {
+        readFile(fileName, { encoding: 'utf8' },
+          (error, data) => {
+            if (error) {
+              reject(error);
+            } else {
+              resolve(data);
+            }
+          });
+      });
+  }
+
   loadFileAsync('test1.json')
   .then(result1 => {
-    //console.log(1, result1);
+
     return loadFileAsync('test2.json');
   })
   .then(result2 => {
-    //console.log(2, result2);
     return loadFileAsync('test3.json');
   })
-  .then(result3 => {
-    //console.log(3, result3);
-  })
+  .then(result3 => {})
   .catch(error => {
-      //console.log(error);
+      console.log(error);
   })
 }
-
 function bench(f) {
   var date = new Date();
-  for (var i = 0; i < 10000; i++) f();
+  for (var i = 0; i < 1000; i++) f();
   return new Date() - date;
 }
 
