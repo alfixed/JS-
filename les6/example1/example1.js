5e896dc772ae81bcf94c281fce1f12bd66f73898
alfixed <alfixed@gmail.com>
2018-05-28 17:22:34 +0300

    Add new version example1

--- les6/example1/example1.js
+++ les6/example1/example1.js
@@ -5,35 +5,52 @@ function tmpSync() {
   const content2 = readFileSync('test2.json', 'utf-8');
   const content3 = readFileSync('test3.json', 'utf-8');
 }
-
+let DateEnd;
 function createReadAsyncWithBenchmark(numberOfFiles) {
-  const start = Date.now();
   let leftFiles = numberOfFiles;
-  return (fileName) => {
+  return (fileName, i) => {
     readFile(fileName,'utf8', (error, text) => {
       if (!error) {
         leftFiles--;
         if (leftFiles === 0) {
+          if(i == 999){
+            DateEnd = new Date();
+          }
         }
       } else {
         console.log('error...', error);
+        DateEnd = new Date();
       }
     })
   }
 }
 
-function tmpAsync() {
+function tmpAsync(i) {
   const readAsync = createReadAsyncWithBenchmark(3);
-  readAsync('test1.json');
-  readAsync('test2.json');
-  readAsync('test3.json');
+  readAsync('test1.json', i);
+  readAsync('test2.json', i);
+  readAsync('test3.json', i);
 }
 
-function bench(f) {
+function benchSync(f) {
   var date = new Date();
   for (var i = 0; i < 1000; i++) f();
   return new Date() - date;
 }
 
-console.log( 'Время синхронного чтения: ' + bench(tmpSync) + 'мс' );
-console.log( 'Время асинхронного чтения: ' + bench(tmpAsync) + 'мс' );
+function benchAsync(f) {
+  let DateStart = new Date();
+   for (let i = 0; i < 1000; i++) {
+      f(i);
+   }
+
+  var timerId = setInterval(function() {
+    if (DateEnd) {
+      clearInterval(timerId);
+      console.log('Время асинхронного чтения: ' + (DateEnd - DateStart) + 'мс');
+    }
+  }, 100);
+}
+
+console.log( 'Время синхронного чтения: ' + benchSync(tmpSync) + 'мс' );
+benchAsync(tmpAsync);
